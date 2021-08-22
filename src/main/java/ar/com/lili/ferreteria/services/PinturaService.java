0113e39511cc2e99abac306f4880174e4455d8d8
LAYALA09 <ayalilian2018@gmail.com>
2021-08-22 18:42:05 -0300

    se agrega  anotación faltante

--- src/main/java/ar/com/lili/ferreteria/services/PinturaService.java
+++ src/main/java/ar/com/lili/ferreteria/services/PinturaService.java
@@ -2,6 +2,7 @@ package ar.com.lili.ferreteria.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -10,7 +11,7 @@ import ar.com.lili.ferreteria.repos.PinturaRepository;
 
 @Service
 public class PinturaService {
-
+    @Autowired 
     private PinturaRepository repo;
     
     public void crear(Pintura pintura){
